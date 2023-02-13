21902f94f23fbb62f76896a4b39b8356fb9d1021
Keeno <160424@student.horizoncollege.nl>
2023-02-13 11:46:18 +0100

    Progress so far

---
+++ createaccount.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Create account</title>
+</head>
+<body>
+
+<form class="form" method="post">
+
+  <label for="name">Naam:</label><br>
+  <input type="text" id="naam" name="naam"><br><br>
+
+  <label for="adres">Adres</label><br>
+  <input type="text" id="adres" name="adres"><br><br>
+
+  <label for="poscode">Postcode</label><br>
+  <input type="text" id="poscode" name="poscode"><br><br>
+
+  <label for="plaats">Plaats</label><br>
+  <input type="text" id="plaats" name="plaats"><br><br>
+
+  <label for="gebdatum">Geboorte Datum</label><br>
+  <input type="text" id="gebdatum" name="gebdatum"><br><br>
+
+  <input type="submit" value="Submit">
+</form> 
+
+</body>
+</html>
--- dbconnection.php
+++ dbconnection.php
@@ -8,7 +8,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
 
-  echo "Connected successfully";
+  // echo "Connected successfully";
 
   $conn -> set_charset("utf8");
 
