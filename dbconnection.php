3ca90711a7fd0404b1435d2d2333ec94242110ed
Keeno <160424@student.horizoncollege.nl>
2023-02-13 10:32:12 +0100

    Project start
    first commit

---
+++ dbconnection.php
@@ -0,0 +1,15 @@
+<?php
+
+ $conn = new mysqli('localhost', 'root', '', 'mkproject'); //localhost connectie
+
+// Check connection
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  echo "Connected successfully";
+
+  $conn -> set_charset("utf8");
+
+  ?>
