57fe2034d6b1fe06a4b1e285c8e7418cc3599c04
maikavijn <vijnmaika@gmail.com>
2023-02-13 10:41:30 +0100

    form toegevoegd

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
 include 'dbconnection.php';
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Index </title>
+</head>
+<body>
+
+<form>
+    <label for="user"> Gebruikersnaam </label><br>
+  <input type="text" id="gebruikersnaam" name="user"><br>
+  <label for="pass"> Wachtwoord </label><br>
+  <input type="text" id="wachtwoord" name="user">
+</form>
+    
+</body>
+</html>
