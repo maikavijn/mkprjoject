170c394e09b3b81e3d9322ad2b22d34bc5db71f1
maikavijn <vijnmaika@gmail.com>
2023-02-13 11:26:07 +0100

    include toegevoegd

--- index.php
+++ index.php
@@ -12,16 +12,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 <?php
 include 'dbconnection.php';
+include 'login.php';
 ?>
-
-<form>
-    <label for="name"> Naam </label><br>
-  <input type="text" id="inputname" name="user"><br>
-  <label for="pass"> Wachtwoord </label><br>
-  <input type="password" id="wachtwoord" name="user"><br><br>
-
-  <button type="button" href="check.php"> Log in </button>
-</form>
     
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form class="login" action="check.php" method="post">
+    
+  <label for="name"> Naam </label><br>
+  <input type="text" id="inputname" name="user"><br>
+
+  <label for="pass"> Wachtwoord </label><br>
+  <input type="password" id="wachtwoord" name="user"><br><br>
+
+  <input type="submit" name="submit" value="Log in">
+</form>
+    
+</body>
+</html>
